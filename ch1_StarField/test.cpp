882764f8b49e14577c72aef7bf162090688566f6
Ankur <arsenic.secondary@gmail.com>
2025-06-17 15:55:15 +0530

    CH1 Starfield: render stars as lines

--- ch1_StarField/test.cpp
+++ ch1_StarField/test.cpp
@@ -3,16 +3,18 @@
 /*
  *
  * this is an attempt to re-create starfield coding challenge by
- * The coding train, but rendered in cpp
+ * The coding train, but rendered in cpp.
+ *
+ *
+ * This by no means is a clean code and is only created to be used as a
+ * playground to play around with SDL3
+ *
  *
  */
 
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
-/* debugging stuff, remove later */
-#include <iostream>
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
@@ -23,7 +25,8 @@ constexpr int win_height = 640;
 
 constexpr unsigned int stars_count = 1000;
 static SDL_FPoint stars_init_coordinates[stars_count];
-static SDL_FPoint stars_final_coordinate[stars_count];
+static SDL_FPoint stars_prev_coordinates[stars_count];
+static SDL_FPoint stars_final_coordinates[stars_count];
 int stars_speed_factor[stars_count];
 
 SDL_FPoint center_it(const SDL_FPoint point) {
@@ -50,7 +53,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     /*  coordinates are from -screen limit to +screen limit */
     stars_init_coordinates[i].x = SDL_rand(2 * win_width) - (win_width);
     stars_init_coordinates[i].y = SDL_rand(2 * win_height) - (win_height);
-    //    stars_init_coordinates[i] = center_it(stars_init_coordinates[i]);
+    stars_prev_coordinates[i] = center_it(stars_init_coordinates[i]);
     stars_speed_factor[i] = SDL_rand(win_width) + 1;
   }
 
@@ -67,9 +70,6 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 ///////// program stuff
 
-// bool draw_circle(void *renderer, const SDL_FPoint center, const float radius,
-//                  const Uint8 r, const Uint8 g, const Uint8 b) {}
-
 float map(float i, float x1, float y1, float x2, float y2) {
   return ((y2 - x2) * ((i - x1) / (y1 - x1)));
 }
@@ -79,32 +79,39 @@ float map(float i, float x1, float y1, float x2, float y2) {
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
 
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0,
+                         SDL_ALPHA_OPAQUE); /* black, full alpha */
+  SDL_RenderClear(renderer);
+
   /* move the stars */
   for (auto i = 0u; i < stars_count; ++i) {
-    stars_final_coordinate[i].x =
-        map(stars_init_coordinates[i].x / stars_speed_factor[i], 0, 1, 0,
-            win_width);
-    stars_final_coordinate[i].y =
-        map(stars_init_coordinates[i].y / stars_speed_factor[i], 0, 1, 0,
-            win_height);
-    stars_final_coordinate[i] = center_it(stars_final_coordinate[i]);
-    stars_speed_factor[i]--;
+    stars_speed_factor[i] -= 5;
 
     /* reset stars which are out of window */
     if (stars_speed_factor[i] < 1) {
       stars_init_coordinates[i].x = SDL_rand(2 * win_width) - (win_width);
       stars_init_coordinates[i].y = SDL_rand(2 * win_height) - (win_height);
+      stars_prev_coordinates[i] = center_it(stars_init_coordinates[i]);
       stars_speed_factor[i] = SDL_rand(win_width) + 1;
     }
-  }
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0,
-                         SDL_ALPHA_OPAQUE); /* black, full alpha */
-  SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255,
-                         SDL_ALPHA_OPAQUE); /* white, full alpha */
-  SDL_RenderPoints(renderer, stars_final_coordinate,
-                   SDL_arraysize(stars_final_coordinate));
+    stars_final_coordinates[i].x =
+        map(stars_init_coordinates[i].x / stars_speed_factor[i], 0, 1, 0,
+            win_width);
+    stars_final_coordinates[i].y =
+        map(stars_init_coordinates[i].y / stars_speed_factor[i], 0, 1, 0,
+            win_height);
+    stars_final_coordinates[i] = center_it(stars_final_coordinates[i]);
+
+    // render the line
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255,
+                           SDL_ALPHA_OPAQUE); /* white, full alpha */
+    SDL_RenderLine(renderer, stars_prev_coordinates[i].x,
+                   stars_prev_coordinates[i].y, stars_final_coordinates[i].x,
+                   stars_final_coordinates[i].y);
+
+    stars_prev_coordinates[i] = stars_final_coordinates[i];
+  }
 
   /* put the newly-cleared rendering on the screen. */
   SDL_RenderPresent(renderer);
